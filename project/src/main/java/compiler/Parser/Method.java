57ced59f0dbd8185ee5958ad89c5d39a615c53a8
Albanedns <denisalbane@gmail.com>
2024-03-25 16:52:04 +0100

    corrections

--- project/src/main/java/compiler/Parser/Method.java
+++ project/src/main/java/compiler/Parser/Method.java
@@ -9,7 +9,9 @@ public class Method extends Node{
 
     public Method( String nameMethod,Type returnType, ArrayList<Param> params, ArrayList<BlockInstruction> body){
         super("Method",new ArrayList<>());
-        this.children.add(new Leaf(nameMethod));
+        Node n = new Node("NameMethod",new ArrayList<>());
+        n.children.add(new Leaf(nameMethod));
+        this.children.add(n);
         this.children.add(returnType);
         this.children.addAll(params);
         this.children.addAll(body);
