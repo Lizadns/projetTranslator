390c968d089d2c495795df1e1ada0e63e5f58789
Albanedns <denisalbane@gmail.com>
2024-03-25 15:32:46 +0100

    print

--- project/src/main/java/compiler/Parser/PrintAST.java
+++ project/src/main/java/compiler/Parser/PrintAST.java
@@ -9,19 +9,40 @@ public class PrintAST {
     }
 
     public void print(){
-        printIndent(programNode, 0);
+        String s = printIndent(programNode, 0);
+        System.out.println(s);
     }
 
-    public void printIndent(Node node,int indent){
-        for (int i = 0; i < indent; i++) {
-            System.out.print("   ");
+    public String printIndent(Node node,int indent){
+        if (node == null) {
+            return ""; // Return an empty string if the node is null
         }
-        System.out.println(node);
-        System.out.println("\n");
-        while (node.children!=null && node.children.size()!=0){
-            for (Node child:node.children) {
-                printIndent(child,indent+1);
+        String s = "";
+        for (int i = 0; i < indent; i++) {
+            s = s+"   ";
+        }
+        if (node.children!=null){
+            if (node.children.size()==1){
+                s+=node+","+node.children.get(0).toString()+"\n";
+                if(node.children.get(0).children!=null) {
+                    for (Node n : node.children.get(0).children) {
+                        s += printIndent(n, indent + 1);
+                    }
+                }
+            }
+            else if(node.children.size()!=0){
+                s+=node+"\n";
+                for(Node n: node.children){
+                    s+=printIndent(n,indent+1);
+                }
+            }
+            else{
+                s+=node+"\n";;
             }
         }
+        else{
+            s+=node+"\n";;
+        }
+        return s;
     }
 }
